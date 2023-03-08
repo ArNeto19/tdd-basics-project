757293e19c065e440d032f196f0bad0b224a4533
arneto19 <armandopdneto@gmail.com>
2023-03-08 12:07:00 -0300

    Added pop method and more tests.

--- test/stack.test.js
+++ test/stack.test.js
@@ -12,29 +12,65 @@ class Stack {
     this.top += 1;
     this.items[this.top] = value;
   }
+
+  pop() {
+    delete this.items[this.top];
+    this.top -= 1;
+  }
 }
 
-describe('My Stack', () => {
+describe("My Stack", () => {
   let stack;
 
   beforeEach(() => {
     stack = new Stack();
   });
 
-  it('is created empty', () => {
+  it("is created empty", () => {
     expect(stack.top).toBe(-1);
     expect(stack.items).toEqual({});
   });
 
-  it('can push to the top', () => {
-    stack.push('🥑');
+  it("can push to the top", () => {
+    stack.push("🥑");
     expect(stack.top).toBe(0);
-    expect(stack.peek).toBe('🥑');
+    expect(stack.peek).toBe("🥑");
 
-    stack.push('🌽');
+    stack.push("🌽");
     expect(stack.top).toBe(1);
-    expect(stack.peek).toBe('🌽');
+    expect(stack.peek).toBe("🌽");
   });
 
-  it.todo('can pop off');
+  it("can pop the top item off", () => {
+    stack.push("🥑");
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe("🥑");
+
+    stack.pop();
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+
+  it("handle multiple entries of push and pop", () => {
+    stack.push("🥑");
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe("🥑");
+
+    stack.push("🌽");
+    expect(stack.top).toBe(1);
+    expect(stack.peek).toBe("🌽");
+
+    stack.pop();
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe("🥑");
+
+    stack.push("🍇");
+    stack.push("🥦");
+    expect(stack.top).toBe(2);
+    expect(stack.peek).toBe("🥦");
+
+    stack.pop();
+    expect(stack.top).toBe(1);
+    expect(stack.peek).toBe("🍇");
+  });
 });
